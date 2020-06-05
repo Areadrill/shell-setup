8ec0e67f5b42e1565eba6e91ce25a79beba9e994
Areadrill <jackthemotorcycle@gmail.com>
2020-06-05 12:21:32 +0100

    Update shell-setup.sh

--- shell-setup.sh
+++ shell-setup.sh
@@ -6,4 +6,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-sh -c  $("curl -fsSl https://raw.githubusercontent.com/areadrill/shall-setup/master/.zshrc > ~/.zshrc")  
+sh -c  $("curl -fsSl https://raw.githubusercontent.com/areadrill/shall-setup/master/zshrc > ~/.zshrc")  
